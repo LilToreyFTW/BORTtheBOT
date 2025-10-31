21d3507e607240d553ace2ccbaeb5a2f941954c5
LilToreyFTW <gtagod2020torey@gmail.com>
2025-10-31 14:03:07 -0700

    V1.0.0.0

--- apps/next/next-env.d.ts
+++ apps/next/next-env.d.ts
@@ -1,5 +1,18 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
 
-// NOTE: This file should not be edited
-// see https://nextjs.org/docs/basic-features/typescript for more information.
+// # ADDED: Audio file type declarations for imports from web app
+declare module "*.mp3" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.wav" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.ogg" {
+  const src: string;
+  export default src;
+}
